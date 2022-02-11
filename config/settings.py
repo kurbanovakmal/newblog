f6ddb28b8aaa4bdb6d9e8261debdd4be139ca394
kurbanovakmal <akmal_kurbanov_1997@mail.ru>
2022-02-11 13:52:18 +0300

    herokuga uzgarishlar

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-aqtwd^lo*#cnjwmy$qkmcf9m!2#@m=ya$sp@ad6eo#ey&m#go&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
