59161573dfe7466136f597cad801c786fc339b52
Акмаль Курбанов <97305653+kurbanovakmal@users.noreply.github.com>
2022-02-24 13:37:44 +0300

    циклы и список

--- new/rrr.py
+++ new/rrr.py
@@ -1,6 +1,12 @@
-name = input('Введите имя:')
-first_name = input('Введите Фамилию:')
-last_name = input('Введите отчество:')
-a = int(input('Введите год поступления:'))
-b = 2 + a
-print(f"{name} {first_name} {last_name} you are going to graduate in {b}")
+# name = input('Введите имя:')
+# first_name = input('Введите Фамилию:')
+# last_name = input('Введите отчество:')
+# a = int(input('Введите год поступления:'))
+# b = 2 + a
+# print(f"{name} {first_name} {last_name} you are going to graduate in {b}")
+
+numbers = input("enter nums using comma: ").split(",")
+sum = 0
+for x in numbers:
+    sum = sum + int(x)**3
+print(sum)
