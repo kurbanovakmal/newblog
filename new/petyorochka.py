a6347e70f44aaed811a1c290d6caa7e95e9bd000
kurbanovakmal <akmal_kurbanov_1997@mail.ru>
2022-04-08 01:45:39 +0300

    class
    rabochiy

--- new/petyorochka.py
+++ new/petyorochka.py
@@ -15,25 +15,30 @@ a = str(random.expovariate(32))
 # print(b)
 
 customers = [{"id": "1212", "name": "akmal", "prods": ["water"]}, {"id": "1212", "name": "roma","prods": ["cola"]}]
-# g = input('cto to xoteli kupit? ')
-# while g == 'da':
-#     prov = input('est account? ')
-#     if prov == 'da':
-#         prov_id = input('vedite id')
-#         for x in customers:
-#             if prov_id == x["id"]:
-#                 new_prods= input('vedite product')
-#                 x["prods"].append(new_prods)
-#                 print(customers)
+g = input('cto to xoteli kupit? ')
+while g == 'da':
+    prov = input('est account? ')
+    if prov == 'da':
+        prov_id = input('vedite id')
+        # if prov_id == 'exit':
+        #     break
+        for x in customers:
+            if prov_id == x["id"]:
+                new_prods= input('vedite product')
+                x["prods"].append(new_prods)
+                print(customers)
+    elif prov == 'net':
+        cust_new = {}
+        cust_new["id"] = a
+        cust_new["name"] = input('vedite imya')
+        cust_new["prods"] = input('vedite producti ').split(", ")
+        customers.append(cust_new)
+        print(customers)
+    elif prov == 'exit':
+        break
 
-#     elif prov == 'net':
-cust_new = [{}]
-for i in cust_new:
-    d = input('vedi emya')
-    # i["id"].append(a)
-    i[0].append(d)
-    
-    print(cust_new)
+
+
 
 
 
