10db5f568ff0fee76ba73142d6258856f3de5724
Акмаль Курбанов <97305653+kurbanovakmal@users.noreply.github.com>
2022-04-07 14:48:21 +0300

    comit to

--- new/petyorochka.py
+++ new/petyorochka.py
@@ -7,7 +7,7 @@ import random
 # print(b)
 # x = input('est account? ')
 
-# a = random.expovariate(32)
+a = str(random.expovariate(32))
 # d = {a:input('vedite imya ')}
 # b.append(d)
 #
@@ -15,29 +15,25 @@ import random
 # print(b)
 
 customers = [{"id": "1212", "name": "akmal", "prods": ["water"]}, {"id": "1212", "name": "roma","prods": ["cola"]}]
-g = input('cto to xoteli kupit? ')
-while g == 'da':
-    prov = input('est account? ')
-    if prov == 'da':
-        prov_id = input('vedite id')
-        for x in customers:
-            if prov_id == x["id"]:
-                new_prods= input('vedite product')
-                x["prods"].append(new_prods)
-                print(customers)
+# g = input('cto to xoteli kupit? ')
+# while g == 'da':
+#     prov = input('est account? ')
+#     if prov == 'da':
+#         prov_id = input('vedite id')
+#         for x in customers:
+#             if prov_id == x["id"]:
+#                 new_prods= input('vedite product')
+#                 x["prods"].append(new_prods)
+#                 print(customers)
 
-    elif prov == 'net':
-        for i in customers:
-            i[0,1,2] = cust_new[0,1,2]
-            cust_new = {a: input('vedite imya, i product').split(", ")}
-        
-            customers.append(cust_new)
-            customer_prod = {a: [input('vedite product').split()]}
-            product.append(customer_prod)
-            print(product)
-
-
-
+#     elif prov == 'net':
+cust_new = [{}]
+for i in cust_new:
+    d = input('vedi emya')
+    # i["id"].append(a)
+    i[0].append(d)
+    
+    print(cust_new)
 
 
 
