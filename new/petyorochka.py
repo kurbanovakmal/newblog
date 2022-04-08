faffbc5d16644b096b0a74333fe8d3e4e2afc684
kurbanovakmal <akmal_kurbanov_1997@mail.ru>
2022-04-08 16:12:50 +0300

    finish kod

--- new/petyorochka.py
+++ new/petyorochka.py
@@ -1,40 +1,28 @@
 import random
-
-# b = []
-# b.append({a: 'qwqwqwq'})
-#
-# product = []
-# print(b)
-# x = input('est account? ')
-
 a = str(random.expovariate(32))
-# d = {a:input('vedite imya ')}
-# b.append(d)
-#
-# print(d.keys())
-# print(b)
-
-customers = [{"id": "1212", "name": "akmal", "prods": ["water"]}, {"id": "1212", "name": "roma","prods": ["cola"]}]
-g = input('cto to xoteli kupit? ')
-while g == 'da':
-    prov = input('est account? ')
-    if prov == 'da':
-        prov_id = input('vedite id')
-        # if prov_id == 'exit':
-        #     break
+product = [{"id":"1", "price": "50","name":"cola" },{"id":"2", "price": "40","name":"pepsi" }]
+customers = [{"id": "1212", "name": "akmal", "prods": ["water"]}, {"id": "12121", "name": "roma","prods": ["cola"]}]
+g = input('Do you want to buy something? ')
+while g == 'Yes' or g == 'yes':
+    summ = 0
+    prov = input("do you have an account? ")
+    if prov == "yes" or prov == "Yes":
+        prov_id = input('Enter your id: ')
+        if prov_id == 'exit':
+            break
         for x in customers:
             if prov_id == x["id"]:
-                new_prods= input('vedite product')
+                new_prods= input("Choose a product: ")
                 x["prods"].append(new_prods)
                 print(customers)
-    elif prov == 'net':
+    elif prov == "No" or prov == "no":
         cust_new = {}
         cust_new["id"] = a
-        cust_new["name"] = input('vedite imya')
-        cust_new["prods"] = input('vedite producti ').split(", ")
+        cust_new["name"] = input("Enter a name: ")
+        cust_new["prods"] = input("Choose a product: ").split(", ")
         customers.append(cust_new)
         print(customers)
-    elif prov == 'exit':
+    elif prov == "exit":
         break
 
 
