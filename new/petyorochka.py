3770340b485460b3613ff537e1754bf9e4cab935
Rakhmatullo <raxmatshamsiddinov@gmail.com>
2022-04-14 01:10:13 +0300

    comit finish
    full

--- new/petyorochka.py
+++ new/petyorochka.py
@@ -1,10 +1,8 @@
 import random
 a = str(random.expovariate(32))
-product = [{"id":"1", "price": "50","name":"cola" },{"id":"2", "price": "40","name":"pepsi" }]
-customers = [{"id": "1212", "name": "akmal", "prods": ["water"]}, {"id": "12121", "name": "roma","prods": ["cola"]}]
+customers = [{"id": "1212", "name": "akmal","age":23, "prods": ["water"]}, {"id": "12121", "name": "roma","prods": ["cola"]}]
 g = input('Do you want to buy something? ')
 while g == 'Yes' or g == 'yes':
-    summ = 0
     prov = input("do you have an account? ")
     if prov == "yes" or prov == "Yes":
         prov_id = input('Enter your id: ')
@@ -15,10 +13,14 @@ while g == 'Yes' or g == 'yes':
                 new_prods= input("Choose a product: ")
                 x["prods"].append(new_prods)
                 print(customers)
+            elif prov_id != x["id"]:
+                print("error!! Please enter correct id! ")
+
     elif prov == "No" or prov == "no":
         cust_new = {}
         cust_new["id"] = a
         cust_new["name"] = input("Enter a name: ")
+        cust_new["age"] = int(input("Enter your age: "))
         cust_new["prods"] = input("Choose a product: ").split(", ")
         customers.append(cust_new)
         print(customers)
